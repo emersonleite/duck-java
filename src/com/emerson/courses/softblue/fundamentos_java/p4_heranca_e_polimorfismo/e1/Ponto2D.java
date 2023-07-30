42aec52ecf338b4705de9db7d7186d4e4b17ad46
Emerson Leite <emerson.bl@gmail.com>
2023-07-30 13:09:34 -0300

    chore: exercise 01 - herança e polimorfismo

---
+++ src/com/emerson/courses/softblue/fundamentos_java/p4_heranca_e_polimorfismo/e1/Ponto2D.java
@@ -0,0 +1,26 @@
+package com.emerson.courses.softblue.fundamentos_java.p4_heranca_e_polimorfismo.e1;
+
+public class Ponto2D {
+
+  private double x;
+  private double y;
+
+  public Ponto2D(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  @Override
+  public String toString() {
+    return "x = " + x + ", y = " + y;
+  }
+
+}
