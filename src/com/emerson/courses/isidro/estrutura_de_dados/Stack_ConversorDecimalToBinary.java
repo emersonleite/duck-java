5ba10df271ba50b6fe39dc0ff70e22503eb64875
Emerson Leite <emerson.bl@gmail.com>
2023-07-30 10:48:03 -0300

    chore: isidro course files

---
+++ src/com/emerson/courses/isidro/estrutura_de_dados/Stack_ConversorDecimalToBinary.java
@@ -0,0 +1,32 @@
+package com.emerson.courses.isidro.estrutura_de_dados;
+
+import java.util.Scanner;
+
+public class Stack_ConversorDecimalToBinary {
+
+  public static void main(String[] args) {
+
+    /* Lendo o valor */
+    Scanner in = new Scanner(System.in);
+    System.out.printf("Entre com o valor inteiro:  ");
+    int number = in.nextInt();
+    in.close();
+
+    Stack p = new Stack(100);
+    int rest;
+
+    while (number != 0) {
+      rest = number % 2;
+      p.push(rest);
+      number = number / 2;
+    }
+
+    while (!p.isEmpty()) {
+      rest = p.pop();
+      System.out.println(rest);
+    }
+
+    System.out.println("\nFim do programa");
+
+  }
+}
