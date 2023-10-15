00ce7bc95c0845611af0e30e4cad46c07e4945df
Emerson Leite <emerson.bl@gmail.com>
2023-10-15 11:36:24 -0300

    chore: rename constants

--- src/com/emerson/courses/cod3r/java_completo/fundamentos/Temperatura.java
+++ src/com/emerson/courses/cod3r/java_completo/fundamentos/Temperatura.java
@@ -5,14 +5,14 @@ public class Temperatura {
     public static void main(String[] args) {
 
         double grausFahrenheit = 86;
-        final double FATOR1 = 32.0;
-        final double FATOR2 = (double) 5 / 9; // Casting
+        final double AJUSTE = 32.0;
+        final double FATOR = (double) 5 / 9; // Casting
 
         // ou
         // double fator2 = 5.0 / 9;
 
 
-        double grausCelsius = (grausFahrenheit - FATOR1) * FATOR2;
+        double grausCelsius = (grausFahrenheit - AJUSTE) * FATOR;
 
         System.out.println("Graus Celsius = " + grausCelsius);
 
