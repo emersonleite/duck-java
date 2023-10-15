cdd3312cdea413b01177a83012f29edecabceddf
Emerson Leite <emerson.bl@gmail.com>
2023-10-15 14:13:26 -0300

    chore: edit comments

--- src/com/emerson/courses/cod3r/java_completo/fundamentos/TipoString.java
+++ src/com/emerson/courses/cod3r/java_completo/fundamentos/TipoString.java
@@ -11,7 +11,7 @@ public class TipoString {
         // Se precisar de transformação, recorrer a outra variável, ou re atribuir a própria variável
 
 
-        String s = "Boa tarde";s
+        String s = "Boa tarde";
         System.out.println(s.concat("!!!"));
         System.out.println(s.startsWith("Boa"));
         System.out.println(s.length());
