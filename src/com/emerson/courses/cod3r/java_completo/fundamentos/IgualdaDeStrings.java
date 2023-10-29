f999ccbf15cafa6143e7e013c8febba8fb20e03b
Emerson Leite <emerson.bl@gmail.com>
2023-10-28 21:55:00 -0300

    chore: update IgualdaDeStrings

--- src/com/emerson/courses/cod3r/java_completo/fundamentos/IgualdaDeStrings.java
+++ src/com/emerson/courses/cod3r/java_completo/fundamentos/IgualdaDeStrings.java
@@ -3,9 +3,17 @@ package com.emerson.courses.cod3r.java_completo.fundamentos;
 public class IgualdaDeStrings {
 
     public static void main(String[] args) {
+
+        // true
+        System.err.println("2" == "2");
+
         String s = new String("2");
 
+        // false
+        System.out.println(s == "2");
+
         // equals
+        // true
         System.out.println("2".equals(s));
 
     }
