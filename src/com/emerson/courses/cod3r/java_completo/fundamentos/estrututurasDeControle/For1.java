7184a47a353ab773d412d57ea60766a9048b8530
Emerson Leite <emerson.bl@gmail.com>
2023-10-28 21:56:10 -0300

    chore: update For1

--- src/com/emerson/courses/cod3r/java_completo/fundamentos/estrututurasDeControle/For1.java
+++ src/com/emerson/courses/cod3r/java_completo/fundamentos/estrututurasDeControle/For1.java
@@ -4,8 +4,16 @@ public class For1 {
 
     public static void main(String[] args) {
 
-        for(int contador = 0; contador <= 2000; contador++){
+        for (int contador = 0; contador <= 5; contador++) {
             System.out.println("Bom dia Zé " + contador);
         }
+
+        /* Outra forma de fazer o for, mas através de um array */
+        int[] idades = { 32, 33, 35 };
+
+        for (int idade : idades) {
+            System.out.println(idade);
+        }
+
     }
 }
